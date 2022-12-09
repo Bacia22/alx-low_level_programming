212dde2492b3354ec40283598b84dce2ff83f156
Bacia22 <adolfobacia@yahoo.com>
2022-12-09 12:44:35 -0800

    Taske

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,11 +2,11 @@
 
 /**
 * main - prints alpha in lower case
-* Return: Always 0 (Success)
+* Return: Always 0
 */
 int main(void)
 {
-char cl:
+char cl;
 for (cl = 'a'; cl <= 'z'; cl++)
 putchar(cl);
 putchar('\n');
