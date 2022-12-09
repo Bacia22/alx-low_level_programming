cbb9f3c93ad1e9e3c59baee1d2efcc3ef0f090f6
Bacia22 <adolfobacia@yahoo.com>
2022-12-09 05:27:53 -0800

    Posit or Negt

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,29 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
 /**
- *main - prints if the number is posetive,negative or 0
- *Return: ALways 0
+ *main - prints if the number is posetive, negative or 0
+ *
+ *Return: ALways 0 (Success)
+ *
  */
 int main(void)
 {
   int n;
+  int num;
 
   srand(time(0));
-  n = rand () - RAND_MAX / 2;
-
-  if (n > 0)
+  n = rand() - RAND_MAX / 2;
+  printf("Last digit  of %d is ", n);
+  num = n % 10;
+  if (num > 5)
     {
-      printf("%d is positive\n", n);
+      printf("%d and is greater than 5\n", num);
     }
-  else if (n == 0)
+  else  if ((num < 6) &&  (num < 0))
     {
-      printf("%d is zero\n", n);
+      printf("%d and is less than 6 and not 0\n", num);
     }
   else
     {
-      printf("% is negative\n", n);
+      printf("%d and is 0\n", num);
     }
   return (0);
 }
