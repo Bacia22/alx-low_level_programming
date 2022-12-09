d1aa86732ca2e39c32fa759f80944e9ab6a89482
Bacia22 <adolfobacia@yahoo.com>
2022-12-09 13:15:23 -0800

    Taske 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+* main - prints the alphabet in lowrcase
+* followed by a new line, except p and e
+* Return: Always 0
+*/
+int main(void)
+{
+char la;
+for (la = 'a'; la <= 'z'; la++)
+if (la != 'e' && la != 'q')
+putchar(la);
+putchar('\n');
+return (0);
+}
