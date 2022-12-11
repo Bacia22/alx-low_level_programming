16f1afa7ee747fd8007314d058ca355770bdcb7f
Bacia22 <adolfobacia@yahoo.com>
2022-12-11 11:41:42 -0800

    Task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,10 +6,10 @@
 */
 int main(void)
 {
-int n;
-for (n = 0; n < 10; n++)
-putchar(n + '0');
-if (n != 9)
+int a;
+for (a = 0; a < 10; a++)
+putchar(a + '0');
+if (a != 9)
 putchar(',');
 putchar(' ');
 putchar('\n');
