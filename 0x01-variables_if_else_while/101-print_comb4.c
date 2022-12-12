4998e1584c06bd2615ac26daf4abc90b3630f3ad
Bacia22 <adolfobacia@yahoo.com>
2022-12-12 13:13:06 -0800

    Task 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -6,15 +6,17 @@
 */
 int main(void)
 {
-int one = '0';
+int ones = '0';
 int tens = '0';
 int hundreds = '0';
 for (hundreds = '0'; hundreds <= '9'; hundreds++)
 {
 for (tens = '0'; tens <= '9'; tens++)
 {
-for (one = '0'; one <= '9'; one++)
-if (!((ones == tens) || (tens == hundreds) || (tens > ones) || (hundreds > tens)))
+for (ones = '0'; ones <= '9'; ones++)
+{
+if (!((ones == tens) || (tens == hundreds) ||
+(tens > ones) || (hundreds > tens)))
 {
 putchar(hundreds);
 putchar(tens);
@@ -29,5 +31,5 @@ putchar(' ');
 }
 }
 putchar('\n');
-return(0);
+return (0);
 }
