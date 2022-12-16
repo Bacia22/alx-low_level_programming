5f60a69964818a956bb17aabc16e974a02606b03
Bacia22 <adolfobacia@yahoo.com>
2022-12-16 04:56:15 -0800

    0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -6,8 +6,15 @@
 */
 int main(void)
 {
-int i;
-i = 0;
-positive_or_negative(i);
+int n;
+int lastNum;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if (n < 0)
+printf("%d is negative\n", n);
+else if (n == 0)
+printf("%d is zero\n", n);
+else
+printf("%d is positive\n", n);
 return (0);
 }
