1110e7e83cbca478420e8b21bb02138e76cdd2d4
Bacia22 <adolfobacia@yahoo.com>
2022-12-19 10:57:31 -0800

    Task 3

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "mian.h"
+/**
+* _strlen - return the length of a string
+* @s: string
+* Return: length
+*/
+int _strlen(char *s)
+{
+int len = 0;
+while (*s != '\0')
+{
+len++;
+s++;
+}
+return (len);
