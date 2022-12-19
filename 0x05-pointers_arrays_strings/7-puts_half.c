fb01995ca13e74af0af7c1fae04554871db13389
Bacia22 <adolfobacia@yahoo.com>
2022-12-19 13:07:00 -0800

    7-puts_half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* puts_half - prints half of s string
+* @str: input
+*/
+void puts_half(char *str)
+{
+int len = 0;
+while (*str != '\0')
+{
+len++;
+str++;
+}
+str -= (len / 2);
+while (*str != '\0')
+{
+_putchar(*str);
+str++;
+}
+_putchar('\n');
+}
