60ea04c45de9b2c832ff60e13bfcae2912a87828
Bacia22 <adolfobacia@yahoo.com>
2022-12-22 11:22:45 -0800

    changes

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -9,7 +9,7 @@ char *string_toupper(char *str)
 int index = 0;
 while (str[index++])
 {
-if (str[index] >= '0' && str[index] <= 'z')
+if (str[index] >= 'a' && str[index] <= 'z')
 str[index] -= 32;
 }
 return (str);
