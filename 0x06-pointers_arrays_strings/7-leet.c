56377b6e4e60ef325979771c3ac70778348ed6cb
Bacia22 <adolfobacia@yahoo.com>
2022-12-22 14:02:00 -0800

    7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -12,8 +12,7 @@ while (str[++index1])
 {
 for (index2 = 0; index2 <= 7; index2++)
 {
-if (str[index1] == leet[index2] ||
-str[index1] - 32 == leet[index2])
+if (str[index1] == leet[index2] || str[index1] - 32 == leet[index2])
 str[index1] = index2 + '0';
 }
 }
