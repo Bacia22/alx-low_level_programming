ad38afd4f50b4dbba301a2c62b31a2f265141462
Bacia22 <adolfobacia@yahoo.com>
2022-12-22 14:08:58 -0800

    7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,13 +8,14 @@ char *leet(char *str)
 {
 int index1 = 0, index2;
 char leet[8] = {'0', 'L', '?', 'E', 'A', '?', '?', 'T'};
-while (str[++index1])
+while (str[index1])
 {
 for (index2 = 0; index2 <= 7; index2++)
 {
 if (str[index1] == leet[index2] || str[index1] - 32 == leet[index2])
 str[index1] = index2 + '0';
 }
+index1++;
 }
 return (str);
 }
