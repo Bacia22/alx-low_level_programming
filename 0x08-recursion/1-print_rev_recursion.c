01f3a0e58b85af7fb650226f8cbef0476beec90a
Bacia22 <adolfobacia@yahoo.com>
2023-01-04 09:07:13 -0800

    1-print_rev_recursion.c

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* _print_rev_recursion - prints a string, followed by a new line
+* @s: pointer to the string
+* Return: void
+*/
+void _print_rev_recursion(char *s)
+{
+if (*s > '\0')
+{
+_print_rev_recursion(s + 1);
+_putchar(*s);
+}
+}
