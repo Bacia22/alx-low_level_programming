61013f03af81daae6a43b6de9eabe759988a8063
Bacia22 <adolfobacia@yahoo.com>
2023-01-05 10:51:26 -0800

    6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -9,6 +9,7 @@
 int is_prime_number(int n)
 {
 return (check_prime(n, 2));
+}
 /**
 * check_prime - check all number < n if they can divide it
 * @n: int
@@ -23,5 +24,4 @@ else if (n % resp == 0 || n <= 1)
 return (0);
 else
 return (check_prime(n, resp + 1));
-}
 }
