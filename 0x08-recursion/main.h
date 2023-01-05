d06f9b64796e597c143b683dee9ef041a7d2ba63
Bacia22 <adolfobacia@yahoo.com>
2023-01-05 11:54:56 -0800

    main.h

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -17,5 +17,7 @@ int is_prime_number(int n);
 int check_prime(int n, int resp);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
+int check(char *s, int start, int end, int pair);
+int last_index(char *s);
 
 #endif
