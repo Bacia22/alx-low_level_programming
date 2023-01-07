584ab00ed422b7c43dddb8fc0011dcfb7d8cf623
Bacia22 <adolfobacia@yahoo.com>
2023-01-06 17:32:13 -0800

    main.h

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - write a program that prints the number of argumrnts passed into it
+*
+* @args: this is the argument count
+* @argv: this is the argument vector
+*
+* Return: The return 0
+*/
+
+int main(int args, char *argv[])  
+{
+printf("%d\n", argv - 1);
+(void)argv;
+return (0);
+}    
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -4,5 +4,7 @@
 int _putchar(char c);
 int check_num(char *str);
 int main(int argc, char *argv[]);
+int main(int args, char *argv[]);
+
 
 #endif
