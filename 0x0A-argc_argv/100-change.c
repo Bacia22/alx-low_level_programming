bdbf95632423a09e10fa808e3aec3f05d4c1e6c3
Bacia22 <adolfobacia@yahoo.com>
2023-01-11 03:17:46 -0800

    100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -5,7 +5,7 @@
 * of maney
 * @argc: number of command line arguments
 * @argv: array that contains the program command line arguments
-* Return: o success
+* Return: 0 - success
 */
 int main(int argc, char *argv[])
 {
