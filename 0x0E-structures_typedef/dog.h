5fa48181ff13248fa4cceec9954db15fa3a9d378
Bacia22 <adolfobacia@yahoo.com>
2023-01-16 15:11:19 -0800

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -23,5 +23,6 @@ void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(char *name, float age, char *owner);
 char *_strcpy(char *dest, char *src);
+void free_dog(dog_t *d);
 int _strlen(char *s);
 #endif
