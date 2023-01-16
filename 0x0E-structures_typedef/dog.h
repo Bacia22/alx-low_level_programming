513dcf00406313402cccb0056f6dc5c91b62ec37
Bacia22 <adolfobacia@yahoo.com>
2023-01-16 15:16:53 -0800

    5-free_dog.c

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -21,7 +21,6 @@ typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(char *name, float age, char *owner);
 char *_strcpy(char *dest, char *src);
 void free_dog(dog_t *d);
 int _strlen(char *s);
