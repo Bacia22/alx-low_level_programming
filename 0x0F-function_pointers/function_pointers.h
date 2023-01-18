c990ae2456fa237f2b66455cd05c6e4d14281127
Bacia22 <adolfobacia@yahoo.com>
2023-01-18 12:16:40 -0800

    2-int_index.c

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,9 @@
 #ifndef function_pointer_h
 #define function_pointer_h
+#include <stddef.h>
+#include <stdlib.h>
 
+int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
