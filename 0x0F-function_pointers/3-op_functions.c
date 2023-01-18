6a5adf3af6475fe6c8feb76a3759f89be34e72e6
Bacia22 <adolfobacia@yahoo.com>
2023-01-18 13:40:50 -0800

    3-calc.h

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,62 +1,21 @@
-#include "3-calc.h"
+#ifndef CALC_H
+#define CALC_H
 
+/**
+* struct op - A struct op.
+* @op: The operator.
+* @f: The associated function.
+*/
+typedef struct op
+{
+char *op;
+int (*f)(int a, int b);
+} op_t;
 int op_add(int a, int b);
 int op_sub(int a, int b);
 int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
-/**
-* op_add - Returns the sum of two numbers.
-* @a: The first number.
-* @b: The second number.
-*
-* Return: The sum of a and b.
-*/
-int op_add(int a, int b)
-{
-return (a + b);
-}
-/**
-* op_sub - Returns the difference of two numbers.
-* @a: The first number.
-* @b: The second number.
-*
-* Return: The difference of a and b.
-*/
-int op_sub(int a, int b)
-{
-return (a - b);
-}
-/**
-* op_mul - Returns the product of two numbers.
-* @a: The first number.
-* @b: The second number.
-*
-* Return: The product of a and b.
-*/
-int op_mul(int a, int b)
-{
-return (a * b);
-}
-/**
-* op_div - Returns the division of two numbers.
-* @a: The first number.
-* @b: The second number.
-*
-* Return: The quotient of a and b.
-*/
-int op_div(int a, int b)
-{
-return (a / b);
-}
-/**
-* op_mod - Returns the remainder of the division of two numbers.
-* @a: The first number.
-* @b: The second number.
-*
-* Return: The remainder of the division of a by b.
-*/
-int op_mod(int a, int b)
-{
-return (a % b);
-}
+int (*get_op_func(char *s))(int, int);
+
+#endif
