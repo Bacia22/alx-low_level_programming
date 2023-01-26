5b5394cd6d923e71b65f8dc2cf3c36c9f58b1989
Bacia22 <adolfobacia@yahoo.com>
2023-01-26 14:16:27 -0800

    0-print_list.c

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -6,19 +6,19 @@
 #include <string.h>
 
 /**
- * struct list_s - singly linked list
- * @str: string - (malloc'ed atring)
- * @len: length of thr string
- * @next: points to next to the next node
- *
- * Description: singly linked list node structure
- * for holberton project
- */
+* struct list_s - singly linked list
+* @str: string - (malloc'ed atring)
+* @len: length of thr string
+* @next: points to next to the next node
+*
+* Description: singly linked list node structure
+* for holberton project
+*/
 typedef struct list_s
 {
-  char *str;
-  unsigned int len;
-  struct list_s *next;
+char *str;
+unsigned int len;
+struct list_s *next;
 } list_t;
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
@@ -26,4 +26,4 @@ list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 
-#endifx
+#endif
