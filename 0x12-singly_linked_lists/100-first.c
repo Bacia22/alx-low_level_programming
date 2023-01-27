21ed443c9b56c488895bf08ab4ecb1e6a74f36ac
Bacia22 <adolfobacia@yahoo.com>
2023-01-27 08:42:29 -0800

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -7,6 +7,6 @@
 
 void __attribute__ ((constructor)) bmain()
 {
-	printf("you're beat! and yet, you must allow");
-		printf(",\nl bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow");
+		printf(",\nI bore my house upon my back!\n");
 }
