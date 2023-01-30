cea7e5970cdc81f32fdddac5555c7ef8b431ba2d
Bacia22 <adolfobacia@yahoo.com>
2023-01-30 14:04:50 -0800

    lists.h

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -14,7 +14,8 @@ typedef struct listint_s
 {
 int n;
 struct listint_s *next;
-}listint_s;
+}
+listint_t;
 int _putchar(char c);
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
