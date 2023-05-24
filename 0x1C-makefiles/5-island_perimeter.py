c65c421a6cbc6946219025f1990b9725501948a2
Bacia22 <adolfobacia@yahoo.com>
2023-05-24 14:22:43 -0700

    5-island_perimeter.py

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -3,12 +3,12 @@
 
 
 def island_perimeter(grid):
-    """this returns the perimeter of the island described in grid"""
+    """that returns the perimeter of the island described in grid"""
     count = 0
     for column in range(len(grid)):
         for row in range(len(grid[column])):
             if grid[column][row] == 1:
-                #verify up
+                # verify up
                 if column - 1 < 0:
                     count += 1
                 else:
@@ -17,28 +17,28 @@ def island_perimeter(grid):
                             count += 1
                     except:
                         pass
-                    #verify down
-                    if column + 1 > len(grid) - 1:
-                        count += 1
-                    else:
-                        try:
-                            if grid[column + 1][row] == 0:
-                                count += 1
-                        except:
-                            pass
-                        #verify right
-                        if row + 1 > len(grid[column]) - 1:
+                # verify down
+                if column + 1 > len(grid) - 1:
+                    count += 1
+                else:
+                    try:
+                        if grid[column + 1][row] == 0:
                             count += 1
-                        else:
-                            try:
-                                if grid[column][row + 1] == 0:
-                                    count += 1
-                            except:
-                                pass
-                            #verify left
-                            try:
-                                if grid[column][row - 1] == 0 or row - 1 < 0:
-                                    count += 1
-                            except:
-                                pass
-                            return count
+                    except:
+                        pass
+                # verify right
+                if row + 1 > len(grid[column]) - 1:
+                    count += 1
+                else:
+                    try:
+                        if grid[column][row + 1] == 0:
+                            count += 1
+                    except:
+                        pass
+                # verify left
+                try:
+                    if grid[column][row - 1] == 0 or row - 1 < 0:
+                        count += 1
+                except:
+                    pass
+    return count
