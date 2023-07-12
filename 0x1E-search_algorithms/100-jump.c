6e6fd0de9f741e8ea97ee247ffefbc9567fc8867
Bacia22 <adolfobacia@yahoo.com>
2023-07-12 12:54:52 -0700

    100-jump.c

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -3,14 +3,14 @@
 
 /**
  * jump_search - searches for a value in an array of
- * integers using the jump search algorithm
+ * integers using the Jump search algorithm
  *
- * @array: inout array
+ * @array: input array
  * @size: size of the array
  * @value: value to search in
  * Return: index of the number
  */
-in jump_search(int *array, size_t size, int value)
+int jump_search(int *array, size_t size, int value)
 {
 	int index, m, k, prev;
 
@@ -22,7 +22,7 @@ in jump_search(int *array, size_t size, int value)
 	prev = index = 0;
 
 	do {
-		print("value checked array[%d] = [%d]\n", index, array[index]);
+		printf("Value checked array[%d] = [%d]\n", index, array[index]);
 
 		if (array[index] == value)
 			return (index);
@@ -31,14 +31,14 @@ in jump_search(int *array, size_t size, int value)
 		index = k * m;
 	} while (index < (int)size && array[index] < value);
 
-	printf("Value found between indexes [%d and [%d]\n", prev, index);
+	printf("Value found between indexes [%d] and [%d]\n", prev, index);
 
 	for (; prev <= index && prev < (int)size; prev++)
 	{
-		printf("Value checked array[%d]\n", prev, array[prev]);
+		printf("Value checked array[%d] = [%d]\n", prev, array[prev]);
 		if (array[prev] == value)
 			return (prev);
 	}
 
-	return (-1)
+	return (-1);
 }
